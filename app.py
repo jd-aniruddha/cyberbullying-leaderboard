498960c27950989160d9186651ea06f5cce09510
jd-aniruddha <aniruddhaj2005@gmail.com>
2026-04-27 12:50:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pandas as pd
 import json
 from sklearn.metrics import accuracy_score, f1_score
 
 app = Flask(__name__)
+CORS(app)
 
 # 🔥 Model logic
 def predict(glucose):
